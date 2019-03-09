d7c3c725fd2c17f9d2c18336f9b3deffc2187917
arielbk <buchwald.kearney@gmail.com>
2019-03-09 14:06:30 +0100

    I created a graphql node!

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,43 @@
  */
 
 // You can delete this file if you're not using it
+
+const axios = require('axios');
+const crypto = require('crypto');
+
+exports.sourceNodes = async ({ actions }) => {
+  const { createNode } = actions;
+
+  // fetch data from datamuse api
+  const fetchRhymingWord = () => axios.get(`https://api.datamuse.com/words?rel_rhy=forgetful`)
+  // await results
+  const res = await fetchRhymingWord();
+  console.log(res.data);
+  // process results and create nodes
+  res.data.map((rhyme, i) => {
+    const rhymeNode = {
+      id: `${i}`,
+      parent: `__SOURCE__`,
+      internal: {
+        type: `Rhyme`,
+      },
+      children: [],
+
+      // Other fields to query
+      word: rhyme.word,
+      score: rhyme.score,
+      numSyllables: rhyme.numSyllables
+    }
+
+    // Content digest for the node (required)
+    const contentDigest = crypto
+      .createHash(`md5`)
+      .update(JSON.stringify(rhymeNode))
+      .digest(`hex`);
+    // Add content digest to the node
+    rhymeNode.internal.contentDigest = contentDigest;
+
+    // Finally, create the node
+    createNode(rhymeNode);
+  });
+}
